3e762b9c0e10e7a55844b27f0685f2e7327d7147
hlinawondwossen <hlinawondwossen2016@gmail.com>
2024-05-19 14:26:26 -0700

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
+""" Exporting csv files"""
+
 import requests
 
 def number_of_subscribers(subreddit):
-    # Define the base URL and headers for the Reddit API request
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'python:subreddit.subscriber.count:v1.0 (by /u/yourusername)'}
-    
+    headers = {'User-Agent': 'python:subreddit.subscriber.count:v1.0 (by /u/Southern_Budget4358)'}
+
     try:
-        # Make a GET request to the Reddit API
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # If the response status code is not 200, return 0
+
         if response.status_code != 200:
             return 0
-        
-        # Parse the JSON response
+
         data = response.json()
-        
-        # Return the number of subscribers
+
         return data['data']['subscribers']
-    
-    except:
-        # If there is any error, return 0
+
+    except Exception as e:
         return 0
+
